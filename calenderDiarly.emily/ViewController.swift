e8621aa47c49e57b6f788733f64ffad3a09763e7
appleEmily <emiri3128@gmail.com>
2019-10-12 21:47:56 +0900

    あとちょっと

--- calenderDiarly.emily/ViewController.swift
+++ calenderDiarly.emily/ViewController.swift
@@ -12,6 +12,9 @@ import CalculateCalendarLogic
 
 class ViewController: UIViewController,FSCalendarDelegate,FSCalendarDataSource,FSCalendarDelegateAppearance {
     
+    var selectedDate: (Int,Int,Int) = (0,0,0)
+    var touched: Bool = false
+    
     @IBOutlet weak var calender: FSCalendar!
     
     override func viewDidLoad() {
@@ -20,27 +23,27 @@ class ViewController: UIViewController,FSCalendarDelegate,FSCalendarDataSource,F
         self.calender.dataSource = self
         // Do any additional setup after loading the view.
     }
-
+    
     fileprivate let gregorian: Calendar = Calendar(identifier: .gregorian)
     fileprivate lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
         return formatter
     }()
-
+    
     // 祝日判定を行い結果を返すメソッド(True:祝日)
     func judgeHoliday(_ date : Date) -> Bool {
         //祝日判定用のカレンダークラスのインスタンス
         let tmpCalendar = Calendar(identifier: .gregorian)
-
+        
         // 祝日判定を行う日にちの年、月、日を取得
         let year = tmpCalendar.component(.year, from: date)
         let month = tmpCalendar.component(.month, from: date)
         let day = tmpCalendar.component(.day, from: date)
-
+        
         // CalculateCalendarLogic()：祝日判定のインスタンスの生成
         let holiday = CalculateCalendarLogic()
-
+        
         return holiday.judgeJapaneseHoliday(year: year, month: month, day: day)
     }
     // date型 -> 年月日をIntで取得
@@ -51,20 +54,20 @@ class ViewController: UIViewController,FSCalendarDelegate,FSCalendarDataSource,F
         let day = tmpCalendar.component(.day, from: date)
         return (year,month,day)
     }
-
+    
     //曜日判定(日曜日:1 〜 土曜日:7)
     func getWeekIdx(_ date: Date) -> Int{
         let tmpCalendar = Calendar(identifier: .gregorian)
         return tmpCalendar.component(.weekday, from: date)
     }
-
+    
     // 土日や祝日の日の文字色を変える
     func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
         //祝日判定をする（祝日は赤色で表示する）
         if self.judgeHoliday(date){
             return UIColor.red
         }
-
+        
         //土日の判定を行う（土曜日は青色、日曜日は赤色で表示する）
         let weekday = self.getWeekIdx(date)
         if weekday == 1 {   //日曜日
@@ -73,10 +76,42 @@ class ViewController: UIViewController,FSCalendarDelegate,FSCalendarDataSource,F
         else if weekday == 7 {  //土曜日
             return UIColor.blue
         }
-
+        
         return nil
     }
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition){
+        
+        selectedDate = self.getDay(date)
+        print(selectedDate)
 
-
+        
+        //print(selectedDate)
+    }
+    func calendar(_ calender: FSCalendar, imageFor date: Date) -> UIImage? {
+        let day = self.getDay(date)
+        self.calender.reloadData()
+        
+                if  selectedDate == day {
+                    //touched = true
+                    return UIImage(named: "flower.png")
+                } else {
+                    return nil
+                }
+//        if touched == true {
+//            return UIImage(named: "flower.png")
+//        }
+//        return nil
+    }
+    
+    //    func calendar(_ calendar: FSCalendar, imageFor date: Date) -> UIImage? {
+    //       let day = getDay(date)
+    //        if  selectedDate == day {
+    //                   print(day)
+    //                   return UIImage(named: "flower.png")
+    //               } else {
+    //                   return nil
+    //               }
+    //    }
+    
 }
 
